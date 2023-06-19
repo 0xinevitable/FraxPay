4f4aef69666031a0e5961b070fc51cdcbf916488
Junho Yeo <i@junho.io>
2023-06-20 06:23:17 +0900

    Setup TailwindCSS

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app';
 import React from 'react';
 
 import { GlobalStyle } from '@/components/GlobalStyle';
+import '@/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
