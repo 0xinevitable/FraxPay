f0fba93e55106112186345a065a29be7bfb79ce5
Junho Yeo <i@junho.io>
2023-06-21 19:14:55 +0900

    Disable select in `OnrampCard`

--- src/components/OnrampCard.tsx
+++ src/components/OnrampCard.tsx
@@ -9,7 +9,7 @@ type FieldItemProps = {
 export const FieldItem: React.FC<FieldItemProps> = ({ field, children }) => {
   return (
     <div className="flex flex-col gap-1 pr-6">
-      <span className="font-medium text-sm text-slate-300">{field}</span>
+      <span className="text-sm font-medium text-slate-300">{field}</span>
       <span className="flex items-center text-base text-slate-200 font-bold h-[24px]">
         {children}
       </span>
@@ -33,7 +33,7 @@ export const OnrampCard: React.FC<OnrampCardProps> = ({
   return (
     <li
       className={clsx(
-        'px-5 py-4 bg-zinc-800 rounded-xl flex flex-col gap-2 transition-all cursor-pointer',
+        'px-5 py-4 bg-zinc-800 rounded-xl flex flex-col gap-2 transition-all cursor-pointer select-none',
         'hover:bg-zinc-700/60 hover:translate-y-[-16px] hover:backdrop-blur-sm hover:shadow-2xl hover:shadow-black/40 hover:z-30',
         className,
       )}
@@ -41,7 +41,7 @@ export const OnrampCard: React.FC<OnrampCardProps> = ({
     >
       <span className="flex items-center gap-2">
         <img src={src} alt={name} className="w-6 h-6 rounded-full" />
-        <h3 className="text-slate-100 font-bold text-lg">{name}</h3>
+        <h3 className="text-lg font-bold text-slate-100">{name}</h3>
       </span>
       <span className="text-slate-400">
         Bank, Wire, and International Options
