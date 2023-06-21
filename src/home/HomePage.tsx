6ba7b24c6e4b37644bd9c694a936068c0dcf4196
Junho Yeo <i@junho.io>
2023-06-21 12:34:31 +0900

    Add change from token button

--- src/home/HomePage.tsx
+++ src/home/HomePage.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { ChevronsRight } from 'lucide-react';
+import { ChevronDown, ChevronsRight } from 'lucide-react';
 import React from 'react';
 
 const HomePage = () => {
@@ -25,7 +25,7 @@ const HomePage = () => {
         </div>
 
         <div className="flex items-center">
-          <div className="flex flex-col items-center py-3 flex-1 gap-1 bg-zinc-800 rounded-xl">
+          <div className="flex flex-col items-center py-3 flex-1 gap-1 bg-zinc-700 hover:bg-zinc-600 rounded-xl relative transition-colors cursor-pointer">
             <TokenLogo
               src={{
                 token: '/assets/eth.png',
@@ -34,16 +34,25 @@ const HomePage = () => {
               alt={{ token: 'Ethereum', network: 'Optimism' }}
             />
             <div className="mt-1 flex flex-col items-center gap-0.5">
-              <span className="text-slate-400 inline-block leading-tight">
+              {/* <button className=" bg-zinc-700 hover:bg-zinc-600 px-2 py-1 rounded-lg shadow-xl shadow-black/40 z-20 flex items-center gap-1 transition-colors">
+                <span className="text-slate-300 inline-block leading-tight">
+                  Ethereum
+                </span>
+                <ChevronDown size={16} className="text-slate-300" />
+              </button> */}
+              <span className="text-slate-300 inline-block leading-tight">
                 Ethereum
               </span>
               <span className="text-slate-200 leading-tight inline-block">
                 0.1 ETH
               </span>
             </div>
+            <span className="bg-zinc-500 text-zinc-50 text-sm font-semibold inline-block leading-tight absolute right-[-4px] top-[-4px] rounded-xl px-2 py-1 shadow-xl shadow-black/20">
+              Change
+            </span>
           </div>
 
-          <div className="w-8 h-8 mx-[-12px] z-10 rounded-full bg-zinc-700 flex items-center justify-center text-zinc-400 shadow-lg">
+          <div className="w-8 h-8 mx-[-12px] z-10 rounded-full bg-zinc-600 flex items-center justify-center text-zinc-400 shadow">
             <ChevronsRight />
           </div>
 
