7fa63dc9bf6479e64edb25545021d861cb77cc2c
Junho Yeo <i@junho.io>
2023-06-30 09:32:20 +0900

    Initialize `HomePage`

---
+++ src/home/HomePage.tsx
@@ -0,0 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
+import { NextPage } from 'next';
+import React from 'react';
+
+const HomePage: NextPage = () => {
+  return <div className="flex flex-col"></div>;
+};
+
+export default HomePage;
