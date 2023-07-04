4e67251ed856b4a8e8ce511ec79b5935e2329a7b
Junho Yeo <i@junho.io>
2023-07-04 10:37:10 +0900

    Setup Yarn Workspaces

--- interface/src/pay/CreatePaymentLinkPage.tsx
+++ interface/src/pay/CreatePaymentLinkPage.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 
 /* eslint-disable @next/next/no-img-element */
-import { Ghost } from 'lucide-react';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import React, { useState } from 'react';
