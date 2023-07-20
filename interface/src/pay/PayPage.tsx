91304f4672dede186c65d0731818a2d9910f627d
Junho Yeo <i@junho.io>
2023-07-20 18:12:04 +0900

    interface: Accordion PoC

--- interface/src/pay/PayPage.tsx
+++ interface/src/pay/PayPage.tsx
@@ -7,6 +7,12 @@ import React from 'react';
 
 import { OnrampCard } from '@/components/OnrampCard';
 import { ProductCard } from '@/components/ProductCard';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 import { Separator } from '@/components/ui/separator';
 import { cn } from '@/lib/utils';
 
@@ -71,83 +77,110 @@ const PayPage: NextPage = () => {
 
           <Separator className="my-8" />
 
-          <h3 className="text-2xl font-medium text-slate-300">Switch Wallet</h3>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="switch-wallet">
+              <AccordionTrigger>
+                <h3 className="text-2xl font-medium text-slate-300">
+                  Switch Wallet
+                </h3>
+              </AccordionTrigger>
+              <AccordionContent className="flex flex-col w-full">
+                MetaMask?
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
 
-          <h3 className="text-2xl font-medium text-slate-300">Swap to Frax</h3>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="swap-to-frax">
+              <AccordionTrigger>
+                <h3 className="text-2xl font-medium text-slate-300">
+                  Swap to Frax
+                </h3>
+              </AccordionTrigger>
+              <AccordionContent className="flex flex-col w-full">
+                {/* <div className="flex flex-col">
+                  <span>Optimism</span>
+                  <span>Balance: $0</span>
+                </div> */}
 
-          {/* <div className="flex flex-col">
-            <span>Optimism</span>
-            <span>Balance: $0</span>
-          </div> */}
-
-          <div className="flex items-center mt-3">
-            <div
-              className={clsx(
-                'flex flex-col items-center py-3 flex-1 gap-1 bg-zinc-700 rounded-xl relative transition-all cursor-pointer',
-                'hover:bg-zinc-600/80 hover:translate-y-[-16px] hover:backdrop-blur-sm',
-              )}
-            >
-              <TokenLogo
-                src={{
-                  token: '/assets/eth.png',
-                  network: '/assets/optimism.png',
-                }}
-                alt={{ token: 'Ethereum', network: 'Optimism' }}
-              />
-              <div className="mt-1 flex flex-col items-center gap-0.5">
-                {/* <button className="z-20 flex items-center gap-1 px-2 py-1 transition-colors shadow-xl rounded-xl bg-zinc-700 hover:bg-zinc-600 shadow-black/40">
+                <div className="flex items-center mt-3">
+                  <div
+                    className={clsx(
+                      'flex flex-col items-center py-3 flex-1 gap-1 bg-zinc-700 rounded-xl relative transition-all cursor-pointer',
+                      'hover:bg-zinc-600/80 hover:translate-y-[-16px] hover:backdrop-blur-sm',
+                    )}
+                  >
+                    <TokenLogo
+                      src={{
+                        token: '/assets/eth.png',
+                        network: '/assets/optimism.png',
+                      }}
+                      alt={{ token: 'Ethereum', network: 'Optimism' }}
+                    />
+                    <div className="mt-1 flex flex-col items-center gap-0.5">
+                      {/* <button className="z-20 flex items-center gap-1 px-2 py-1 transition-colors shadow-xl rounded-xl bg-zinc-700 hover:bg-zinc-600 shadow-black/40">
                 <span className="inline-block leading-tight text-slate-300">
                   Ethereum
                 </span>
                 <ChevronDown size={16} className="text-slate-300" />
               </button> */}
-                <span className="inline-block leading-tight text-slate-300">
-                  Ethereum
-                </span>
-                <span className="inline-block leading-tight text-slate-200">
-                  0.1 ETH
-                </span>
-              </div>
-              <span className="bg-zinc-500/20 backdrop-blur-sm text-zinc-50/80 text-sm font-semibold inline-block leading-tight absolute right-[-6px] top-[-6px] rounded-xl px-2 py-1 shadow-xl shadow-black/20">
-                Change
-              </span>
-            </div>
+                      <span className="inline-block leading-tight text-slate-300">
+                        Ethereum
+                      </span>
+                      <span className="inline-block leading-tight text-slate-200">
+                        0.1 ETH
+                      </span>
+                    </div>
+                    <span className="bg-zinc-500/20 backdrop-blur-sm text-zinc-50/80 text-sm font-semibold inline-block leading-tight absolute right-[-6px] top-[-6px] rounded-xl px-2 py-1 shadow-xl shadow-black/20">
+                      Change
+                    </span>
+                  </div>
 
-            <div className="w-8 h-8 mx-[-12px] z-10 rounded-full bg-zinc-600 flex items-center justify-center text-zinc-400 shadow">
-              <ChevronsRight />
-            </div>
+                  <div className="w-8 h-8 mx-[-12px] z-10 rounded-full bg-zinc-600 flex items-center justify-center text-zinc-400 shadow">
+                    <ChevronsRight />
+                  </div>
 
-            <div className="flex flex-col items-center flex-1 gap-1 py-3 bg-zinc-800 rounded-xl">
-              <TokenLogo
-                src={{
-                  token: '/assets/frax.png',
-                  network: '/assets/optimism.png',
-                }}
-                alt={{ token: 'Frax', network: 'Optimism' }}
-              />
-              <div className="mt-1 flex flex-col items-center gap-0.5">
-                <span className="inline-block leading-tight text-slate-400">
-                  Frax
-                </span>
-                <span className="inline-block leading-tight text-slate-200">
-                  50.18 FRAX
-                </span>
-              </div>
-            </div>
-          </div>
+                  <div className="flex flex-col items-center flex-1 gap-1 py-3 bg-zinc-800 rounded-xl">
+                    <TokenLogo
+                      src={{
+                        token: '/assets/frax.png',
+                        network: '/assets/optimism.png',
+                      }}
+                      alt={{ token: 'Frax', network: 'Optimism' }}
+                    />
+                    <div className="mt-1 flex flex-col items-center gap-0.5">
+                      <span className="inline-block leading-tight text-slate-400">
+                        Frax
+                      </span>
+                      <span className="inline-block leading-tight text-slate-200">
+                        50.18 FRAX
+                      </span>
+                    </div>
+                  </div>
+                </div>
 
-          <button className="py-3 font-bold transition-colors bg-slate-100 rounded-xl text-zinc-800 hover:bg-slate-300">
-            Continue
-          </button>
+                <button className="w-full py-3 font-bold transition-colors bg-slate-100 rounded-xl text-zinc-800 hover:bg-slate-300">
+                  Continue
+                </button>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
 
-          <h3 className="text-2xl font-medium text-slate-300">
-            Onramp to Frax
-          </h3>
-
-          <ul className="flex flex-col gap-2 mt-3">
-            <OnrampCard src="/assets/stably.png" name="Stably" />
-            <OnrampCard src="/assets/transak.svg" name="Transak" />
-          </ul>
+          <Accordion type="single" collapsible>
+            <AccordionItem value="onramp-to-frax">
+              <AccordionTrigger>
+                <h3 className="text-2xl font-medium text-slate-300">
+                  Onramp to Frax
+                </h3>
+              </AccordionTrigger>
+              <AccordionContent className="flex flex-col w-full">
+                <ul className="flex flex-col gap-2 mt-3">
+                  <OnrampCard src="/assets/stably.png" name="Stably" />
+                  <OnrampCard src="/assets/transak.svg" name="Transak" />
+                </ul>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
         </div>
       </div>
     </div>
